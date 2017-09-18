b5f73558fbff259219689ddff17a5aa77faff23d
stockcj <claudestockton@gmail.com>
2017-09-18 06:43:07 +0100

    now for design...ugh

--- layouts/home.vue
+++ layouts/home.vue
@@ -1,13 +1,27 @@
 <template>
   <v-app light>
     <v-toolbar :class="[toolbar ? 'toolbar--home' : 'hidden']" fixed>
-      <v-toolbar-title v-text="title"></v-toolbar-title>
+      <img src="/csrlogo1_icon.svg" alt="Cambridge Serviced Rooms" style="height:48px;"/>
       <v-spacer></v-spacer>
+      <v-toolbar-items>
+        <v-btn flat to="/reviews">Reviews</v-btn>
+        <v-btn flat to="/contact">Contact Us</v-btn>
+      </v-toolbar-items>
     </v-toolbar>
+    <v-parallax src="/backs.jpg" style="height: auto;">
+      <v-layout column align-center justify-center>
+        <img src="/csrlogo1_small.svg" alt="Cambridge Serviced Rooms" class="mt-5 mb-4 hidden-sm-and-up" />
+        <img src="/csrlogo1.svg" alt="Cambridge Serviced Rooms" class="mt-5 mb-4 hidden-xs-only" />
+        <div class="text-xs-center mb-5">
+          <h2 class="white--text">Cambridge Serviced Rooms</h2>
+          <v-flex xs8 offset-xs2>
+            <h4 class="white--text"><span class="thin">We offer</span> fully serviced, centrally located, luxury accommodation<span class="thin"> in the city of Cambridge.</span></h4>
+          </v-flex>
+        </div>      
+      </v-layout>
+    </v-parallax>
     <main class="home">
-      <v-container fluid class="primary-layout">
-        <nuxt />
-      </v-container>
+      <nuxt />
     </main>
     <v-footer>
       <span>&copy; 2017</span>
@@ -31,7 +45,7 @@
     },
     methods: {
       onScroll () {
-        this.toolbar = window.scrollY > 100
+        this.toolbar = window.scrollY > 400
       }
     }
   }
@@ -40,5 +54,11 @@
 <style>
   .home {
     padding-top: 0px !important;
+  }
+  .title-border {
+    height: 10px;
+    border-top: 1px solid white;
+    border-bottom: 1px solid white;
+    background-color:#476B78;
   }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,36 +1,58 @@
 <template>
-  <v-layout column justify-center align-center>
-    <v-flex xs12>
-      <div class="text-xs-center">
-        <img src="/csrlogo1.svg" alt="Cambridge Serviced Rooms" class="mb-5" />
+  <v-container fluid>
+    <!-- <v-layout column justify-center align-center>
+      <div class="text-xs-center mt-5 mb-5">
+        <v-flex xs8 offset-xs2>
+          <h3 class="white--text">Services</h
+          <h4 class="white--text"><span class="thin">Our properties are the perfect base for</span> working, studying<span class="thin">, or just </span>visiting
+            <span class="thin">Cambridge.</span></h4>
+        </v-flex>
       </div>
-      <v-card>
-        <v-card-title class="headline">Welcome to the Vuetify + Nuxt.js template</v-card-title>
-        <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>For more information on Vuetify, check out the <a href="https://vuetifyjs.com" target="_blank">documentation</a>.</p>
-          <p>If you have questions, please join the official <a href="https://chat.vuetifyjs.com/" target="_blank" title="chat">discord</a>.</p>
-          <p>Find a bug? Report it on the github <a href="https://github.com/vuetifyjs/vuetify/issues" target="_blank" title="contribute">issue board</a>.</p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a href="https://nuxtjs.org/" target="_blank">Nuxt Documentation</a>
-          <br>
-          <a href="https://github.com/nuxt/nuxt.js" target="_blank">Nuxt GitHub</a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn primary flat nuxt to="/reviews">Continue</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-flex>
-  </v-layout>
+    </v-layout> -->
+    <v-layout column>
+      <v-flex xs12 md10 offset-md1>
+        <v-container fluid grid-list-xl class="mt-5">
+          <v-layout row wrap align-items-center> 
+            <v-flex xs12 md5 class="text-xs-center">
+              <h3>Fully Serviced</h3>
+            </v-flex>
+            <v-flex xs12 md7 class="mb-5">
+              <h5 class="thin">We offer fully serviced accommodation to our visitors. All guests can expect meticulously cleaned and prepared bedrooms
+                  and bathrooms, supplied with fresh linen and towels. We then service all rooms on a weekly basis, or prior to a
+                  new arrival, when once again we supply freshly laundered linen and towels.</h5>
+            </v-flex>
+            <v-flex xs12 md5 class="text-xs-center">
+              <h3>Centrally Located</h3>
+            </v-flex>
+            <v-flex xs12 md7 class="mb-5">
+              <h5 class="thin">Our properties are located in quiet residential side streets in the centre of Cambridge, and just minutes from the
+                historic heart of city. Supermarkets, restaurants, shops, and bars, are all within easy reach.</h5>
+            </v-flex>
+            <v-flex xs12 md5 class="text-xs-center">
+              <h3>Excellent Value</h3>
+            </v-flex>
+            <v-flex xs12 md7 class="mb-5">
+              <h5 class="thin">Our fees are all inclusive, with no additional costs incurred during a visit. There are no additional administration
+                fees, gas, water or electricity charges. There are no broadband or TV licence fees, council tax payments, or laundry
+                costs. This makes budgeting for a stay with us very, very, straight forward.</h5>
+            </v-flex>
+            <v-flex xs12 md5 class="text-xs-center">
+              <h3>Exceptional Facilities</h3>
+            </v-flex>
+            <v-flex xs12 md7 class="mb-5">
+              <h5 class="thin">The houses provide free, unlimited, high speed broadband, via wi-fi connection. All guests have use of both the exceptionally
+                equipped kitchens, which enable comfortable self catering, and the utility rooms, which have both a washing machine
+                and tumble drier. The gardens benefits from soft landscaping, furniture, and have secure cycle storage space.</h5>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
-export default {
-  layout: 'home'
-}
+  export default {
+    layout: 'home'
+  }
 </script>
