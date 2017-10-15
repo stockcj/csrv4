f7f78d4e27b36e60a47ea6e43704cc04fef980e7
stockcj <claudestockton@gmail.com>
2017-10-15 20:11:06 +0100

    added hero image to room component

--- components/Room.vue
+++ components/Room.vue
@@ -1,35 +1,52 @@
 <template>
-  <v-container fluid class="primary-layout">
-    <v-layout>
+  <div>
+    <v-layout :style="[heroStyle, {backgroundImage: 'url(' + room.titleImage + ')'}]" align-center>
       <v-flex xs12>
-        <h4 class="page-header white--text text-xs-center mt-5">{{ room.houseName }}</h4>
         <h1 class="page-header white--text text-xs-center">{{ room.name }}</h1>
-        <v-container grid-list-xl class="mt-5">
-          <v-layout row wrap>
-            <v-flex xs12 lg6>
-              <v-carousel hide-controls :class="[$vuetify.breakpoint.xs ? 'carousel--small' : '']">
-                <v-carousel-item v-for="(img,i) in room.images" v-bind:src="img" :key="i"></v-carousel-item>
-              </v-carousel>
-            </v-flex>
-            <v-flex xs12 lg6>
-              <v-card>
-                <v-card-text>
-                  <div class="pa-3">
-                    <p class="headline">{{room.description}}</p>
-                    <p class="title mt-5">From £{{room.cost}} per week</p>
-                  </div>
-                </v-card-text>
-              </v-card>
-            </v-flex>
-          </v-layout>
-        </v-container>
+        <h4 class="page-header white--text text-xs-center mt-5">{{ room.houseName }}</h4>
       </v-flex>
     </v-layout>
-  </v-container>
+    <v-container fluid class="primary-layout">
+      <v-layout>
+        <v-flex xs12>
+          <v-container grid-list-xl class="mt-5">
+            <v-layout row wrap>
+              <v-flex xs12 lg6>
+                <v-carousel hide-controls :class="[$vuetify.breakpoint.xs ? 'carousel--small' : '']">
+                  <v-carousel-item v-for="(img,i) in room.images" v-bind:src="img" :key="i"></v-carousel-item>
+                </v-carousel>
+              </v-flex>
+              <v-flex xs12 lg6>
+                <v-card>
+                  <v-card-text>
+                    <div class="pa-3">
+                      <p class="headline">{{room.description}}</p>
+                      <p class="title mt-5">From £{{room.cost}} per week</p>
+                    </div>
+                  </v-card-text>
+                </v-card>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </div>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      heroStyle: {
+        backgroundPosition: 'center',
+        backgroundSize: 'cover',
+        height: '95vh',
+        width: 'auto',
+        maxWidth: '100%'
+      }
+    }
+  },
   name: 'room',
   computed: {
     room () {
