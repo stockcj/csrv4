dcad19c45dced47f071458ae430f0a29d1e07dc8
stockcj <claudestockton@gmail.com>
2017-11-11 16:41:21 +0000

    added fair street too

--- pages/fair-street/index.vue
+++ pages/fair-street/index.vue
@@ -22,7 +22,9 @@
           <h5 class="white--text text-xs-center thin mt-2">Fair Street is in an ideal location for those visiting the City. The luxury serviced rooms are a mere 10-minute walk from the historic city centre and all that Cambridge has to offer.</h5>
         </v-flex>
         <v-flex xs12>
-          <div class="gmap"></div>
+          <div class="gmap">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d864.43172847973!2d0.12877562045421104!3d52.20737190174702!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47d87093d3c37cd5%3A0x8b2ded7c7b16590c!2s5+Fair+St%2C+Cambridge+CB1+1HA!5e0!3m2!1sen!2suk!4v1510418216181" width="100%" height="600" frameborder="0" style="border:0" allowfullscreen></iframe>
+          </div>
         </v-flex>
         <v-flex xs12 lg6 offset-lg3>
           <hr class="mt-5">
